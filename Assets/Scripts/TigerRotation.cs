7dad54011fd752cd3707ca98700625f5b00ec015
NicolasVPC <NicolasVPC@users.noreply.github.com>
2019-05-13 16:27:14 +0200

    Initial commit

---
+++ Assets/Scripts/TigerRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TigerRotation : MonoBehaviour
+{
+    private float accely = 0;
+    private const float add = 30f;
+    void Update()
+    {
+        accely = Input.acceleration.y+add;
+        this.transform.Rotate(0, accely * Time.deltaTime, 0);
+    }
+}
